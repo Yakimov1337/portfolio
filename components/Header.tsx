042c12c223fd8966830f0ee4ec74ce7841e0d030
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-09-22 15:07:45 +0300

    Tailwind config fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,33 +6,45 @@ type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header className="sticky top-0 p-5 flex-items-start justify-between max-w-7x1 mx-auto z-20 xl:items-center">
-      <motion.div
-        initial={{ x: -500, opacity: 0, scale: 0.5 }}
-        animate={{ x: 0, opacity: 1, scale: 1 }}
-        transition={{ duration: 1.5 }}
-        className="flex flex-row items-center"
-      >
-        <SocialIcon url="" fgColor="gray" bgColor="transparent" />
-        <SocialIcon url="" fgColor="gray" bgColor="transparent" />
-        <SocialIcon url="" fgColor="gray" bgColor="transparent" />
-      </motion.div>
-      <motion.div
-        initial={{ x: -500, opacity: 0, scale: 0.5 }}
-        animate={{ x: 0, opacity: 1, scale: 1 }}
-        transition={{ duration: 1 }}
-        className="flex flex-row items-center text-gray-300 cursor-pointer"
-      >
-        <SocialIcon
-          className="cursor-pointer"
-          network="email"
-          fgColor="gray"
-          bgColor="transparent"
-        />
-        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
-          Get In Touch
-        </p>
-      </motion.div>
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+        <motion.div
+          initial={{ x: -500, opacity: 0, scale: 0.5 }}
+          animate={{ x: 0, opacity: 1, scale: 1 }}
+          transition={{ duration: 1.5 }}
+          className="flex flex-row items-center"
+        >
+          <SocialIcon
+            url="https://www.youtube.com/watch?v=urgi2iz9P6U"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+          <SocialIcon
+            url="https://www.youtube.com/watch?v=urgi2iz9P6U"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+          <SocialIcon
+            url="https://www.youtube.com/watch?v=urgi2iz9P6U"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+        </motion.div>
+        <motion.div
+          initial={{ x: -500, opacity: 0, scale: 0.5 }}
+          animate={{ x: 0, opacity: 1, scale: 1 }}
+          transition={{ duration: 1 }}
+          className="flex flex-row items-center text-gray-300 cursor-pointer"
+        >
+          <SocialIcon
+            className="cursor-pointer"
+            network="email"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+          <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+            Get In Touch
+          </p>
+        </motion.div>
     </header>
   );
 }
