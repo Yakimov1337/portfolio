71f2747dda5149f3b9ed559e7774dc28791ca991
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-09-22 14:19:55 +0300

    responsivness

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,15 +4,22 @@ type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header>
+    <header className="sticky top-0 p-5 flex-items-start justify-between max-w-7x1 mx-auto z-20 xl:items-center">
       <div className="flex flex-row items-center">
         <SocialIcon url="" fgColor="gray" bgColor="transparent" />
         <SocialIcon url="" fgColor="gray" bgColor="transparent" />
         <SocialIcon url="" fgColor="gray" bgColor="transparent" />
       </div>
       <div>
-      <SocialIcon className="cursor-pointer" network="email" fgColor='gray' bgColor='transparent'/>
-      <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get In Touch</p>
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Get In Touch
+        </p>
       </div>
     </header>
   );
