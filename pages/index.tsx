8b1c6163dd33928b336698acde0bc6d953e15739
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-09-27 09:11:22 +0300

    WorkExperience added

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Hero from  "../components/Hero";
 import About from  "../components/About";
 import Header from "../components/Header";
+import WorkExperience from "../components/WorkExperience";
 
 const Home: NextPage = () => {
   return (
@@ -18,6 +19,9 @@ const Home: NextPage = () => {
       </section>
       <section id="about" className="snap-center">
         <About/>
+      </section>
+      <section id="workExperience" className="snap-center">
+        <WorkExperience/>
       </section>
     </div>
   );
