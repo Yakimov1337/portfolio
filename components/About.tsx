3c3ca8292266952c9cd7e9badbe058caefa19033
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-09-27 09:30:29 +0300

    motion added

--- components/About.tsx
+++ components/About.tsx
@@ -7,7 +7,12 @@ type Props = {};
 // https://avatars.githubusercontent.com/u/92337248?v=4
 export default function About({}: Props) {
   return (
-    <div className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         About
       </h3>
@@ -16,8 +21,8 @@ export default function About({}: Props) {
         whileInView={{ opacity: 1, x: 0 }}
         transition={{ duration: 1.2 }}
         viewport={{ once: true }}
-        src="https://avatars.githubusercontent.com/u/92337248?v=4"
-        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]"
+        src="https://cdn.sanity.io/images/ltuexkre/producton/ac8058b25cc880765f6549dd27223349f37a7c2f-1173x1458.jpg"
+        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
       />
       <div className="space-y-10 px-0 md:px-10">
         <h4 className="text-4xl font-semibold ">
@@ -26,15 +31,13 @@ export default function About({}: Props) {
           background
         </h4>
         <p className="text-base">
-          texttexttexttexttexttexttexttex
-          ttexttexttexttexttexttexttexttextte
+          texttexttexttexttexttexttexttex ttexttexttexttexttexttexttexttextte
           xttexttexttexttexttexttexttexttexttextt
           exttexttexttexttexttexttexttexttexttext
           texttexttexttexttexttexttexttexttexttextte
-          xttexttexttexttexttexttexttexttexttexttext
-          texttexttexttexttexttext
+          xttexttexttexttexttexttexttexttexttexttext texttexttexttexttexttext
         </p>
       </div>
-    </div>
+    </motion.div>
   );
 }
