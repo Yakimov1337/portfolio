8c8cbfa5e9f8a02dd12d0297d26a00955b547bfb
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-10-03 09:34:45 +0300

    type definitions

--- typings.d.ts
+++ typings.d.ts
@@ -11,8 +11,53 @@ interface Image {
     _rev: string;
     _updatedAt: string;
   }
-  
 
+export interface PageInfo extends SanityBody{
+    _type: "pageInfo";
+    address: string;
+    backgroundInformation:string;
+    email: string;
+    role: string;
+    heroImage: Image;
+    name:string;
+    phoneNumber:string;
+    profilePic:Image;
+}
+  
+export interface Technology extends SanityBody {
+    _type:"skill";
+    image:Image;
+    progress: number;
+    title:string;
+}
+
+export interface Skills extends SanityBody {
+    _type:"skill";
+    image:Image;
+    progress:number;
+    title:string;
+}
+
+export interface Experience extends SanityBody {
+    _type:"experience";
+    company:string;
+    companyImage:Image;
+    dateStarted:Date;
+    dateEnded:Date;
+    isCurrentlyWorkingHere:boolean;
+    jobTitle:string;
+    points:string[];
+    technologies:Technology[];
+}
+
+export interface Project extends SanityBody{
+    title:string;
+    _type:"project";
+    image:Image;
+    linkToBuild:string;
+    summary:string;
+    technologies: Technology[];
+}
 export interface Social extends SanityBody {
   _type: "social";
   title: string;
