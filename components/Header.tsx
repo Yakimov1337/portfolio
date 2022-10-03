ef51ce53dfa9a061c6c256a66b654254781fee37
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-10-03 11:11:00 +0300

    social icons api

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
-import Link from 'next/link'
+import Link from "next/link";
+import { Social } from "../typings";
 
-type Props = {};
+type Props = {
+  socials: Social[];
+};
 
-export default function Header({}: Props) {
+export default function Header({socials}: Props) {
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
       <motion.div
@@ -14,7 +17,15 @@ export default function Header({}: Props) {
         transition={{ duration: 1.5 }}
         className="flex flex-row items-center"
       >
-        <SocialIcon
+        {socials.map((social)=>(
+          <SocialIcon 
+          key={social._id}
+          url={social.url}
+          fgColor="gray"
+          bgColor="transparent"
+          />
+        ))}
+        {/* <SocialIcon
           url="https://www.youtube.com/watch?v=urgi2iz9P6U"
           fgColor="gray"
           bgColor="transparent"
@@ -28,7 +39,7 @@ export default function Header({}: Props) {
           url="https://www.youtube.com/watch?v=urgi2iz9P6U"
           fgColor="gray"
           bgColor="transparent"
-        />
+        /> */}
       </motion.div>
       <Link href="#contact">
         <motion.div
